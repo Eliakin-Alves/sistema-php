ee0b801124b3afee62e73c0c78aa537479d39a93
Eliakin Alves <eliakin.alves.dev@gmail.com>
2019-10-23 16:35:38 -0300

    versão 4

---
+++ cliente.class.php
@@ -0,0 +1,52 @@
+<?php
+    class Cliente{
+        private $id;
+        private $nome;
+        private $telefone;
+        private $email;
+        private $cpf;
+        public __construct(){
+            
+        }
+
+        public function getId(){
+            return $this->id;
+        }
+    
+        public function setId($id){
+            $this->id = $id;
+        }
+    
+        public function getNome(){
+            return $this->nome;
+        }
+    
+        public function setNome($nome){
+            $this->nome = $nome;
+        }
+    
+        public function getTelefone(){
+            return $this->telefone;
+        }
+    
+        public function setTelefone($telefone){
+            $this->telefone = $telefone;
+        }
+    
+        public function getEmail(){
+            return $this->email;
+        }
+    
+        public function setEmail($email){
+            $this->email = $email;
+        }
+    
+        public function getCpf(){
+            return $this->cpf;
+        }
+    
+        public function setCpf($cpf){
+            $this->cpf = $cpf;
+        }
+    }
+?>
---
+++ cliente.dao.php
@@ -0,0 +1,8 @@
+<?php
+    include "cliente.class.php";
+    class DAOCliente {
+        public function cadastrar(Cliente $cliente) {
+
+        }
+    }
+?>
