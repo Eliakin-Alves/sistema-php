78ee11fa519a37724970d51a190a6b2b1844e936
Eliakin Alves <eliakin.alves.dev@gmail.com>
2019-10-23 16:05:58 -0300

    versão 2

--- calculadora.class.php
+++ calculadora.class.php
@@ -3,6 +3,9 @@
         public $n1;
         public $n2;
 
+        public function __construct() {
+            echo "Calcular <br />";
+        }
         public function calcularSoma() {
             return $this->n1 + $this->n2;
         }
--- cliente.class.php
+++ cliente.class.php
@@ -1,9 +1,26 @@
 <?php
 class cliente{
-    public $id;
-    public $nome;
-    public $telefone;
-    public $email;
-    public $cpf;
+    private $id;
+    private $nome;
+    private $telefone;
+    private $email;
+    private $cpf;
+    public function setEmail($email) {
+        if(filter_var($email, FILTER_VALIDATE_EMAIL)) {
+            $this->email = $email;
+        }
+        else{
+            throw new Exception('E-mail Inválido');
+        } 
+    }
+    public function getEmail() {
+        return $this->email;
+    }
 }
+$obj = new Cliente();
+$obj->setEmail("eli@email.com"); // com encapsulamento
+echo $obj->getEmail(); // com encapsulamento
+echo "<br/>";
+$obj->email = "eli@email.com"; // sem encapsulamento
+echo $obj->email; // sem encapsulamento
 ?>
